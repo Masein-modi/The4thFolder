f4c1e17addc94ce1fe718a29fa8dbd7f2599dae1
Masein Mody <maseinmody@Maseins-MacBook-Air.local>
2017-07-18 19:25:58 -0700

    code added v.01

--- MiraPills/MiraPills/ViewController.swift
+++ MiraPills/MiraPills/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let numbers = ["1" , "2" , "3","4","5","6","7","8"]
+    
+    @IBOutlet weak var numberPicker: UIPickerView!
+    @IBOutlet weak var numberPickerBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        numberPicker.dataSource = self
+        numberPicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +27,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return numbers.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return numbers[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        numberPickerBtn.setTitle(numbers[row], for: UIControlState())
+        numberPicker.isHidden = true
+    }
+    @IBAction func numberBtnPressed(_ sender: Any) {
+        numberPicker.isHidden = false
+    }
 
 }
 
