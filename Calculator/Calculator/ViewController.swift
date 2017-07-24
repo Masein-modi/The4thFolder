d150fe1a568d7b6bd2b43ca6224b58e68009d395
Masein Mody <maseinmody@Maseins-MacBook-Air.local>
2017-07-23 23:08:44 -0700

    calculator sound-v.01 added

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var btnsound : AVAudioPlayer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do{
+            try btnsound = AVAudioPlayer(contentsOf: soundURL)
+            btnsound.prepareToPlay()
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+
+    }
+    
+    @IBAction func numberPressed(sender : UIButton){
+        playSound()
     }
 
+    func playSound(){
+        if btnsound.isPlaying {
+            btnsound.stop()
+        }
+        btnsound.play()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
