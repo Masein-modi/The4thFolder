0611b3135ae0bf7aded412c319e28df236466de9
Masein Mody <maseinmody@Maseins-MacBook-Air.local>
2017-07-30 05:14:24 -0700

    calculator sound-v.01 added

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,7 +11,23 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resetBtn: UIButton!
+    @IBOutlet weak var lblOutbut: UILabel!
     var btnsound : AVAudioPlayer!
+    
+    enum Operations :String {
+        case Empty = ""
+        case Devide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+    }
+    
+    var runningNum = ""
+    var lftVar = ""
+    var rwtVar = ""
+    var currentOp = Operations.Empty
+    var result = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,15 +45,70 @@ class ViewController: UIViewController {
 
     }
     
-    @IBAction func numberPressed(sender : UIButton){
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        runningNum = "\(sender.tag)"
+        lblOutbut.text = runningNum
         playSound()
+        resetBtn.isHidden = false
     }
+    @IBAction func eqPressed(_ sender: UIButton){
+        operateOperation(operation: currentOp)
+ 
+    }
+    @IBAction func adPressed(_ sender: UIButton){
+        operateOperation(operation: .Add)
+    }
+    @IBAction func suPressed(_ sender: UIButton){
+        operateOperation(operation: .Subtract)
+    }
+    @IBAction func dePressed(_ sender: UIButton){
+        operateOperation(operation: .Devide)
+    }
+    @IBAction func muPressed(_ sender: UIButton){
+        operateOperation(operation: .Multiply)
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        resetBtn.isHidden = true
+        lblOutbut.text = "0"
+        lftVar = ""
+        currentOp = Operations.Empty
+        runningNum = ""
+    }
+    
 
     func playSound(){
         if btnsound.isPlaying {
             btnsound.stop()
         }
         btnsound.play()
+    }
+    
+    
+    func operateOperation(operation : Operations){
+        if currentOp != Operations.Empty{
+            switch currentOp {
+            case Operations.Add:
+                result = "\(Double(lftVar)! + Double(runningNum)!)"
+            case Operations.Subtract:
+                result = "\(Double(lftVar)! - Double(runningNum)!)"
+            case Operations.Devide:
+                result = "\(Double(lftVar)! / Double(runningNum)!)"
+            case Operations.Multiply:
+                result = "\(Double(lftVar)! * Double(runningNum)!)"
+            default : print("")
+            }
+            lftVar = result
+            lblOutbut.text = lftVar
+            currentOp = operation
+            
+        }else{
+            lftVar = runningNum
+            runningNum = ""
+            currentOp = operation
+        }
+        resetBtn.isHidden = false
     }
     
     override func didReceiveMemoryWarning() {
